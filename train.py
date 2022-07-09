1bed88e0739350e7f6061e42224117de74e9786f
Plutonium-239 <plutonium.239.811@gmail.com>
2022-07-09 18:17:55 +0530

    tensorboard

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import time
 import os
 import subprocess # for uploading tensorboard
 from torch.profiler import profile, record_function, ProfilerActivity
+import matplotlib.pyplot as plt
 
 previous_runs = os.listdir('runs')
 if len(previous_runs) == 0:
@@ -31,7 +32,7 @@ segclip, preproc, preproc_lbl = model.load_custom_clip('RN50', num_classes=5, de
 segclip.to(device) # redundant
 
 dataset = pascalVOCLoader(config['pascal_root'], preproc, preproc_lbl, split=config['mode'], img_size=224, is_transform=True)
-trainloader = DataLoader(dataset, batch_size=config['batch_size'])
+trainloader = DataLoader(dataset, batch_size=config['batch_size'], pin_memory=True, num_workers=4)
 
 
 loss_fn = nn.CrossEntropyLoss()
@@ -65,33 +66,42 @@ pascal_labels = [template+x for x in pascal_labels]
 pascal_labels.insert(0, '')
 text_tokens = clip.tokenize(pascal_labels).to(device)
 final_loss = 0
-with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
-	with record_function("model_inference"):
-		for epoch in tqdm(range(config['num_epochs'])):
-			# tqdm.write(f'Epoch {epoch} started.')
-			epoch_loss = 0
-			pbar = tqdm(enumerate(trainloader), total=len(trainloader), leave=False)
-
-			for i, (batch_img, batch_lbl, texts) in pbar:
-				batch_img, batch_lbl = batch_img.to(device), batch_lbl.to(device)
-				# if i==0:
-				# 	writer.add_graph(segclip, (batch_img, text_tokens))
-				output = segclip(batch_img, text_tokens)
-				loss = loss_fn(output, batch_lbl)
-				loss.backward()
-				optimiser.step()
-				pbar.set_description_str(f'loss: {loss.item()}')
-				epoch_loss += loss.item()
-			epoch_loss /= len(trainloader)
-			tqdm.write(f'Epoch {epoch} loss: {epoch_loss}')
-			if epoch%10==9:
-				writer.add_scalar('training loss', epoch_loss, epoch)
-				final_loss = epoch_loss.item()
+# with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+	# with record_function("model_inference"):
+for epoch in tqdm(range(config['num_epochs'])):
+	# tqdm.write(f'Epoch {epoch} started.')
+	epoch_loss = 0
+	pbar = tqdm(enumerate(trainloader), total=len(trainloader), leave=False)
+	t_sum = 0
+	last_time = time.time()
+	times = []
+	for i, (batch_img, batch_lbl, texts) in pbar:
+		t = time.time()
+		t_diff = t - last_time
+		t_sum += t_diff
+		last_time = t
+		# times.append(t_diff)
+		batch_img, batch_lbl = batch_img.to(device), batch_lbl.to(device)
+		writer.add_scalar(f'Epoch {epoch} dataloader timings', t_diff, i)
+		# if i==0:
+		#   writer.add_graph(segclip, (batch_img, text_tokens))
+		output = segclip(batch_img, text_tokens)
+		loss = loss_fn(output, batch_lbl)
+		loss.backward()
+		optimiser.step()
+		pbar.set_description_str(f'loss: {loss.item()}, batch time: {t_diff}')
+		epoch_loss += loss.item()
+	epoch_loss /= len(trainloader)
+	tqdm.write(f'Epoch {epoch} loss: {epoch_loss}')
+	# plot = plt.plot(times)
+	# if epoch%10==9:
+	writer.add_scalar('training loss', epoch_loss, epoch)
+	final_loss = epoch_loss.item()
 
 print('End')
 print()
-f = open('profile.txt','w')
-f.write(prof.key_averages().table(sort_by="cpu_time_total"))
+# f = open('profile.txt','w')
+# f.write(prof.key_averages().table(sort_by="cpu_time_total"))
 writer.add_hparams(config, {'final loss': final_loss}, run_name=logdir)
 
 
