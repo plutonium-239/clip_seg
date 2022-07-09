1390fd3eea59a468f400ac7112cf38711c92ad07
Plutonium-239 <plutonium.239.811@gmail.com>
2022-07-09 13:29:20 +0530

    tensorboard

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 config = {
 	'mode': 'train',
 	'batch_size': 4,
+	'num_epochs': 100,
 
 	'lr': 1e-3,
 	'wd': 1e-4,
--- train.py
+++ train.py
@@ -7,10 +7,27 @@ from pascal_voc_loader import pascalVOCLoader
 from config import config
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+import time
+import os
+import subprocess # for uploading tensorboard
+
+previous_runs = os.listdir('runs')
+if len(previous_runs) == 0:
+    run_number = 1
+else:
+    run_number = max([int(s.split('run_')[1]) for s in previous_runs]) + 1
+
+logdir = 'run_%02d' % run_number
+
+writer = SummaryWriter('runs/'+logdir)
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Running on', device)
 
 segclip, preproc, preproc_lbl = model.load_custom_clip('RN50', num_classes=5, device=device)
+segclip.to(device) # redundant
 
 dataset = pascalVOCLoader(config['pascal_root'], preproc, preproc_lbl, split=config['mode'], img_size=224, is_transform=True)
 trainloader = DataLoader(dataset, batch_size=config['batch_size'])
@@ -18,8 +35,6 @@ trainloader = DataLoader(dataset, batch_size=config['batch_size'])
 
 loss_fn = nn.CrossEntropyLoss()
 optimiser = torch.optim.Adam(segclip.parameters(), lr=config['lr'], weight_decay=config['wd'])
-
-pbar = tqdm(enumerate(trainloader), total=len(trainloader))
 
 pascal_labels = [
 		'aeroplane',
@@ -46,21 +61,40 @@ pascal_labels = [
 
 template = 'a photo of a '
 pascal_labels = [template+x for x in pascal_labels]
+pascal_labels.insert(0, '')
 text_tokens = clip.tokenize(pascal_labels).to(device)
+final_loss = 0
+for epoch in tqdm(range(config['num_epochs'])):
+	# tqdm.write(f'Epoch {epoch} started.')
+	epoch_loss = 0
+	pbar = tqdm(enumerate(trainloader), total=len(trainloader), leave=False)
+	for i, (batch_img, batch_lbl, texts) in pbar:
+		batch_img, batch_lbl = batch_img.to(device), batch_lbl.to(device)
+		# if i==0:
+		# 	writer.add_graph(segclip, (batch_img, text_tokens))
 
-for i, (batch_img, batch_lbl, texts) in pbar:
-	batch_img, batch_lbl = batch_img.to(device), batch_lbl.to(device)
+		output = segclip(batch_img, text_tokens)
+		# print(output.shape, 
+		# 	batch_img.shape,
+		# 	# output.sum(dim=0).shape, 
+		# 	text_tokens.shape,
+		# 	batch_lbl.shape)
+		# 1024 dimensional embedding for each pixel (or 1024 channels) and 
+		# take dot product of all N classes and take softmax of that
+		loss = loss_fn(output, batch_lbl)
+		loss.backward()
+		optimiser.step()
+		pbar.set_description_str(f'loss: {loss.item()}')
+		epoch_loss += loss.item()
+	epoch_loss /= len(trainloader)
+	tqdm.write(f'Epoch {epoch} loss: {epoch_loss}')
+	if epoch%10==9:
+		writer.add_scalar('training loss', epoch_loss, epoch)
+		final_loss = epoch_loss.item()
 
-	output = segclip(batch_img, text_tokens)
-	print(output.shape, 
-		batch_img.shape,
-		# output.sum(dim=0).shape, 
-		text_tokens.shape,
-		batch_lbl.shape)
-	# 1024 dimensional embedding for each pixel (or 1024 channels) and 
-	# take dot product of all N classes and take softmax of that
-	loss = loss_fn(output, batch_lbl)
-	loss.backward()
-	optimiser.step()
-	pbar.set_postfix({'loss':loss.item()})
-	
+writer.add_hparams(config, {'final loss': final_loss}, run_name=logdir)
+
+
+writer.close()
+torch.save(segclip.state_dict(), f'runs/{logdir}/model.pt')
+subprocess.run(['tensorboard', 'dev', 'upload', '--logdir', 'runs/'+logdir])
