0120675c814f052a36f9fb7153cb33c06bb68736
Plutonium-239 <plutonium.239.811@gmail.com>
2022-07-28 22:42:25 +0530

    add unfrozen separately

---
+++ models/model_unfrozen.py
@@ -0,0 +1,203 @@
+import torch
+import clip
+import cv2
+import torch.nn as nn
+from torchvision.models.segmentation import fcn
+import torchvision.transforms as tf
+
+class Identity(nn.Module):
+	def __init__(self):
+		super(Identity, self).__init__()
+		
+	def forward(self, x):
+		return x
+
+
+class SegCLIP(nn.Module):
+	def __init__(self, clip_encoder, hidden_emb_depth):
+		super().__init__()
+		# self.clip_visual = clip_encoder.visual
+		self.clip = clip_encoder # only for the text part 
+		# self.num_classes = num_classes
+		self.embed_dim = hidden_emb_depth
+		'''
+		Layer (type:depth-idx)                   Output Shape
+		=================================================================
+		ModifiedResNet                           [1, 2048, 7, 7]
+		├─Conv2d: 1-1                            [1, 32, 112, 112]
+		├─BatchNorm2d: 1-2                       [1, 32, 112, 112]
+		├─ReLU: 1-3                              [1, 32, 112, 112]
+		├─Conv2d: 1-4                            [1, 32, 112, 112]
+		├─BatchNorm2d: 1-5                       [1, 32, 112, 112]
+		├─ReLU: 1-6                              [1, 32, 112, 112]
+		├─Conv2d: 1-7                            [1, 64, 112, 112]
+		├─BatchNorm2d: 1-8                       [1, 64, 112, 112]
+		├─ReLU: 1-9                              [1, 64, 112, 112]
+		├─AvgPool2d: 1-10                        [1, 64, 56, 56]
+		├─Sequential: 1-11                       [1, 256, 56, 56]
+		├─Sequential: 1-12                       [1, 512, 28, 28]
+		├─Sequential: 1-13                       [1, 1024, 14, 14]
+		├─Sequential: 1-14                       [1, 2048, 7, 7]
+
+		Removing:
+		├─AttentionPool2d: 1-15                  [1, 2048, 7, 7]
+
+		Adding:
+		├─FCNHead: 1-3                                [1, 1024, 28, 28]         [1, 21, 28, 28]
+		│    └─Conv2d: 2-14                           [1, 1024, 28, 28]         [1, 256, 28, 28]
+		│    └─BatchNorm2d: 2-15                      [1, 256, 28, 28]          [1, 256, 28, 28]
+		│    └─ReLU: 2-16                             [1, 256, 28, 28]          [1, 256, 28, 28]
+		│    └─Dropout: 2-17                          [1, 256, 28, 28]          [1, 256, 28, 28]
+		│    └─Conv2d: 2-18                           [1, 256, 28, 28]          [1, 21, 28, 28]
+
+		'''
+		# to remove the Attention Pool layer (replace with identity)
+		# self.clip_encoder.visual.attnpool = Identity()
+		self.up1 = nn.Upsample(scale_factor=4)
+		self.conv1 = nn.Conv2d(in_channels = 2048, out_channels = 512, kernel_size=3, padding=2, dilation=2, bias=False)
+		self.bn1 = nn.BatchNorm2d(512)
+		self.relu1 = nn.ReLU(inplace=True)
+
+		self.up2 = nn.Upsample(scale_factor=2)
+		self.conv2 = nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size=3, padding=2, dilation=2, bias=False)
+		self.bn2 = nn.BatchNorm2d(256)
+		self.relu2 = nn.ReLU(inplace=True)
+
+		self.up3 = nn.Upsample(scale_factor=4)
+		self.conv3 = nn.Conv2d(in_channels = 256, out_channels = 64, kernel_size=3, padding=2, dilation=2, bias=False)
+		self.bn3 = nn.BatchNorm2d(64)
+		self.relu3 = nn.ReLU(inplace=True)
+		
+		# self.fcnhead = fcn.FCNHead(in_channels = 64, channels = hidden_emb_depth)
+		self.fcnheadconv = nn.Conv2d(in_channels = 64, out_channels = hidden_emb_depth, kernel_size=3, padding=2, dilation=2, bias=False)
+		self.fcnheadbn = nn.BatchNorm2d(hidden_emb_depth)
+
+	def forward(self, image, text):
+		image = image.type(self.clip.visual.conv1.weight.dtype)
+		# identity = image.clone()
+
+		def stem(image):
+			image = self.clip.visual.relu1(self.clip.visual.bn1(self.clip.visual.conv1(image)))
+			image = self.clip.visual.relu2(self.clip.visual.bn2(self.clip.visual.conv2(image)))
+			image = self.clip.visual.relu3(self.clip.visual.bn3(self.clip.visual.conv3(image)))
+			image = self.clip.visual.avgpool(image)
+			return image
+
+		with torch.no_grad():
+			text = self.clip.encode_text(text)
+	
+		image = stem(image)
+		# [1, 64, 56, 56]
+		res = self.clip.visual.layer1(image)
+		# res = image.clone()
+		# [1, 256, 56, 56]
+		res2 = self.clip.visual.layer2(res)
+		# res2 = image.clone()
+		# [1, 512, 28, 28]
+		image = self.clip.visual.layer3(res2)
+		# [1, 1024, 14, 14]
+		image = self.clip.visual.layer4(image)
+		# [1, 2048, 7, 7]
+
+		# removing attnpool
+		# image = self.clip_encoder.visual.attnpool(image)
+		
+		image = self.up1(image)
+		image = self.conv1(image)
+		# [1, 512, 28, 28]
+		image += res2
+		image = self.relu1(self.bn1(image))
+		
+		image = self.up2(image)
+		image = self.conv2(image)
+		# [1, 256, 56, 56]
+		image += res
+		image = self.relu2(self.bn2(image))
+		
+		image = self.up3(image)
+		image = self.conv3(image)
+		# [1, 64, 224, 224]
+		# cannot do this since identity has only 3 channels
+		# image += identity
+		image = self.relu3(self.bn3(image))
+		
+		image = self.fcnheadconv(image)
+		image = self.fcnheadbn(image)
+		# [num_classes, 224, 224]
+
+		'''
+		image -> [1, 1024, 224, 224]
+		text -> [num_classes, 1024]
+
+		we want -> [1, num_classes, 224, 224]
+		'''
+		ish = image.shape
+		# print(ish)
+		image = image.permute(0,2,3,1).reshape(-1, image.shape[1]) # linearize for faster dot product
+		x = image @ text.t() # [1*224*224, 1024] dot [1024, num_classes] = [1*224*224, num_classes]
+		x = x.reshape(ish[0], ish[2], ish[3], -1).permute(0,3,1,2)
+
+		return x
+		
+
+
+
+
+def load_custom_clip(model_name, img_size=224, device=None):
+	'''
+	Function to load 'model_name' from clip and adapt it for segmentation
+	Returns:
+	model: The CLIP model with .visual and .transformer
+	preproc: The preprocessing function reqd by model. Expects a numpy array of [H, W, C]
+	'''
+	assert model_name in clip.available_models(), f"model_name should be one of RNx from {clip.available_models()}, found {model_name}"
+	model, preprocess = clip.load(model_name, device=device)
+	model = model.float()
+	# doing .float() is important otherwise it outputs nans (known issue on github page)
+	'''
+	this is 'preprocess' (torchvision.transforms) for RN50: 
+	Compose(
+		Resize(size=224, interpolation=bicubic)
+		CenterCrop(size=(224, 224))
+		<function _convert_image_to_rgb at 0x000002364D1EE430>
+		ToTensor()
+		Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
+	)
+
+	It expects a PIL image in the 3rd transform (convert to rgb)
+	But since cv2(numpy) is ~1.5x faster than PIL we will use that
+	We can call `cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)` and remove that from preprocess
+	'''
+	embed_dim =  model.text_projection.shape[1]
+	segCLIP = SegCLIP(model, embed_dim)
+	preprocess.transforms.pop(2) # convert_to_rgb
+	preprocess.transforms.pop(2) # ToTensor
+	# preprocess.transforms[0] = tf.Resize(size=224, interpolation=tf.InterpolationMode.BICUBIC)
+	preprocess.transforms[0] = tf.Resize(size=img_size, interpolation=tf.InterpolationMode.BICUBIC)
+	preprocess.transforms[1] = tf.CenterCrop(size=(img_size,img_size))
+	preproc = lambda x: preprocess( tf.ToTensor()(x) )
+	preprocess_label = tf.Compose([])
+	preprocess_label.transforms = preprocess.transforms.copy()
+	# label cant be interpolated normally, use nearest neighbor interpolation to keep integer values
+	# preprocess_label.transforms[0] = tf.Resize(size=224, interpolation=tf.InterpolationMode.NEAREST)
+	preprocess_label.transforms[0] = tf.Resize(size=img_size, interpolation=tf.InterpolationMode.NEAREST)
+	preprocess_label.transforms.pop(-1) # no normalization for label
+	preproc_lbl = lambda x: preprocess_label(torch.tensor(x).unsqueeze(0)).squeeze(0)
+	print(preprocess, preprocess_label)
+
+	return segCLIP.to(device), preproc, preproc_lbl
+
+
+def intersectionAndUnionGPU(output, target, K, ignore_index=255):
+	# 'K' classes, output and target sizes are N or N * L or N * H * W, each value in range 0 to K - 1.
+	assert (output.dim() in [1, 2, 3])
+	assert output.shape == target.shape
+	output = output.view(-1)
+	target = target.view(-1)
+	output[target == ignore_index] = ignore_index
+	intersection = output[output == target]
+	area_intersection = torch.histc(intersection, bins=K, min=0, max=K-1)
+	area_output = torch.histc(output, bins=K, min=0, max=K-1)
+	area_target = torch.histc(target, bins=K, min=0, max=K-1)
+	area_union = area_output + area_target - area_intersection
+	return area_intersection, area_union, area_target
---
+++ train_unfrozen.py
@@ -0,0 +1,198 @@
+from models import model_unfrozen
+import clip
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from pascal_voc_loader import pascalVOCLoader
+from pascal_5i_loader import Pascal5iLoader
+from config import config
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+import time
+import os,sys
+import subprocess # for uploading tensorboard
+from torch.profiler import profile, record_function, ProfilerActivity
+import matplotlib.pyplot as plt
+from models.model import intersectionAndUnionGPU
+
+previous_runs = os.listdir('fewshotruns')
+if len(previous_runs) == 0:
+	run_number = 1
+else:
+	run_number = max([int(s.split('run_')[1]) for s in previous_runs]) + 1
+
+logdir = 'run_%02d' % run_number
+
+writer = SummaryWriter('fewshotruns/'+logdir)
+layout = {
+	"Loss and mIOU for train and val": {
+		"loss": ["Multiline", ["loss/train", "loss/val"]],
+		"miou": ["Multiline", ["miou/train", "miou/val"]],
+	},
+}
+writer.add_custom_scalars(layout)
+
+def norm_im(im):
+	x_min, x_max = im.min(), im.max()
+	ims = (im - x_min) / (x_max-x_min)
+	return ims
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Running on', device, 'logging in', logdir)
+
+segclip, preproc, preproc_lbl = model_unfrozen.load_custom_clip('RN50', device=device, img_size=224)
+segclip.to(device) # redundant
+
+if len(sys.argv)>1:
+	config['fold'] = int(sys.argv[1])
+
+# dataset = pascalVOCLoader(config['pascal_root'], preproc, preproc_lbl, split='train', img_size=224, is_transform=True)
+dataset = Pascal5iLoader(config['pascal_root'], fold=config['fold'], preproc=preproc, preproc_lbl=preproc_lbl)
+trainloader = DataLoader(dataset, batch_size=config['batch_size'], pin_memory=True, num_workers=config['num_workers'])
+
+# valset = pascalVOCLoader(config['pascal_root'], preproc, preproc_lbl, split='val', img_size=224, is_transform=True)
+valset = Pascal5iLoader(config['pascal_root'], fold=config['fold'], preproc=preproc, preproc_lbl=preproc_lbl, train=False)
+valloader = DataLoader(valset, batch_size=config['batch_size'], pin_memory=True, num_workers=config['num_workers'])
+
+loss_fn = nn.CrossEntropyLoss()
+optimiser = torch.optim.Adam(segclip.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+
+pascal_labels = [
+	'aeroplane',
+	'bicycle',
+	'bird',
+	'boat',
+	'bottle',
+	'bus',
+	'car',
+	'cat',
+	'chair',
+	'cow',
+	'dog',
+	'horse',
+	'motorbike',
+	'person',
+	'sheep',
+	'sofa',
+	'diningtable',
+	'pottedplant',
+	'train',
+	'tvmonitor',
+]
+
+template = 'a photo of a '
+pascal_labels = [template+x for x in pascal_labels]
+pascal_labels.insert(0, '')
+pascal_labels_train = [pascal_labels[x] for x in dataset.label_set]
+# pascal_labels_val = [pascal_labels[x] for x in valset.label_set]
+pascal_labels_val = pascal_labels
+pascal_labels_train.insert(0, '')
+# pascal_labels_val.insert(0, '')
+text_tokens_train = clip.tokenize(pascal_labels_train).to(device)
+text_tokens_val = clip.tokenize(pascal_labels_val).to(device)
+print(text_tokens_train.shape)
+print(text_tokens_val.shape)
+final_loss = 0
+final_miou_t = 0
+final_miou_v = 0
+start = time.time()
+
+# with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+# 	with record_function("one epoch total"):
+for epoch in tqdm(range(config['num_epochs'])):
+	# tqdm.write(f'Epoch {epoch} started.')
+	epoch_loss_t = 0
+	epoch_miou_t = 0
+	pbar = tqdm(enumerate(trainloader), total=len(trainloader), leave=False)
+	segclip.train()
+	for i, (batch_img, batch_lbl) in pbar:
+		# times.append(t_diff)
+		batch_img, batch_lbl = batch_img.to(device), batch_lbl.to(device)
+		# if i==0:
+		#   writer.add_graph(segclip, (batch_img, text_tokens))
+		output = segclip(batch_img, text_tokens_train)
+		# print(output.min(), output.max())
+		loss = loss_fn(output, batch_lbl)
+		loss.backward()
+		optimiser.step()
+		batch_pred = F.softmax(output, dim=1).argmax(dim=1)
+		# batch_pred[batch_pred > config['fold']*5] += 5
+		inter,union,_ = intersectionAndUnionGPU(batch_pred, batch_lbl, output.shape[1])
+		batch_miou = (inter.sum()/union.sum()).item()
+		# tqdm.write(str(i)+str(u))
+		epoch_miou_t += batch_miou
+		# tqdm.write(str(batch_miou))
+		# tqdm.write(str((i/u).mean()))		
+		pbar.set_description_str(f'train loss: {loss.item():.4f}, iou: {batch_miou:.4f}')
+		epoch_loss_t += loss.item()
+		# if i==len(trainloader)-1:
+		# 	pred = torch.stack([dataset.decode_segmap(x).permute(2,0,1) for x in batch_pred]).to(device)
+		# 	lbl = torch.stack([dataset.decode_segmap(x).permute(2,0,1) for x in batch_lbl]).to(device)
+		# 	writer.add_images('img + GT', (norm_im(batch_img)*255).int() | lbl.int(), epoch)
+		# 	writer.add_images('img + pred', (norm_im(batch_img)*255).int() | pred.int(), epoch)
+		# 	writer.add_images('img', norm_im(batch_img), epoch)
+		# 	writer.add_images('GT', lbl, epoch)
+		# 	writer.add_images('pred', pred, epoch)
+	epoch_loss_t /= len(trainloader)
+	epoch_miou_t /= len(trainloader)
+
+	segclip.eval()
+	epoch_miou_v = 0
+	epoch_loss_v = 0
+	pbar2 = tqdm(enumerate(valloader), total=len(valloader), leave=False)
+	for i, (batch_img, batch_lbl) in pbar2:
+		# times.append(t_diff)
+		batch_img, batch_lbl = batch_img.to(device), batch_lbl.to(device)
+		# if i==0:
+		#   writer.add_graph(segclip, (batch_img, text_tokens))
+		output = segclip(batch_img, text_tokens_val)
+		# print(output.min(), output.max())
+		loss = loss_fn(output, batch_lbl)
+		batch_pred = F.softmax(output, dim=1).argmax(dim=1)
+		# batch_pred[batch_pred > 0] += config['fold']*5
+		inter,union,_ = intersectionAndUnionGPU(batch_pred, batch_lbl, output.shape[1])
+		batch_miou = (inter.sum()/union.sum()).item()
+		# tqdm.write(str(i)+str(u))
+		epoch_miou_v += batch_miou
+		# tqdm.write(str(batch_miou))
+		# tqdm.write(str((i/u).mean()))		
+		pbar2.set_description_str(f'val loss: {loss.item():.4f}, iou: {batch_miou:.4f}')
+		epoch_loss_v += loss.item()
+		if i==0 and epoch%5==4:
+			pred = torch.stack([dataset.decode_segmap(x).permute(2,0,1) for x in batch_pred]).to(device)
+			lbl = torch.stack([dataset.decode_segmap(x).permute(2,0,1) for x in batch_lbl]).to(device)
+			writer.add_images('img vs pred vs GT', torch.cat([norm_im(batch_img), norm_im(pred), norm_im(lbl)], dim=2), epoch)
+			# writer.add_images('img + GT', (norm_im(batch_img)*255).int() | lbl.int(), epoch)
+			# writer.add_images('img + pred', (norm_im(batch_img)*255).int() | pred.int(), epoch)
+			# writer.add_images('img', norm_im(batch_img), epoch)
+			# writer.add_images('GT', lbl, epoch)
+			# writer.add_images('pred', pred, epoch)
+	epoch_loss_v /= len(valloader)
+	epoch_miou_v /= len(valloader)
+	tqdm.write(f'(train/val) Epoch {epoch} loss: {epoch_loss_t:.4f}/{epoch_loss_v:.4f}, mean mIOU: {epoch_miou_t:.4f}/{epoch_miou_v:.4f}')
+	
+	writer.add_scalar('loss/train', epoch_loss_t, epoch)
+	writer.add_scalar('loss/val', epoch_loss_v, epoch)
+
+	writer.add_scalar('miou/train', epoch_miou_t, epoch)
+	writer.add_scalar('miou/val', epoch_miou_v, epoch)
+	
+	final_miou_t += epoch_miou_t
+	final_miou_v += epoch_miou_v
+	final_loss = epoch_loss_v
+# f = open('profile.txt','w')
+# f.write(prof.key_averages().table(sort_by="cpu_time_total"))
+end = time.time()
+t_diff = end - start
+final_miou_t /= config['num_epochs']
+final_miou_v /= config['num_epochs']
+# writer.add_scalar(f'Epoch time', t_diff, epoch)
+print('End')
+print()
+writer.add_hparams(config, {'mean train mIOU':final_miou_t, 'mean val mIOU':final_miou_v, 'final loss': final_loss, 'total time': t_diff}, run_name='.')
+
+writer.close()
+torch.save(segclip.state_dict(), f'fewshotruns/{logdir}/model.pt')
+# subprocess.run(['tensorboard', 'dev', 'upload', '--logdir', 'runs/'])
