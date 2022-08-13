e5a9ae453df49e7c7883b7256415ae15a9cc9a40
Plutonium-239 <plutonium.239.811@gmail.com>
2022-08-13 16:37:10 +0530

    updates

--- cm.py
+++ cm.py
@@ -1,7 +1,32 @@
 from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
+import argparse
+import plotly.express as px
+import torch
+
+pascal_classes = [
+	'aeroplane',
+	'bicycle',
+	'bird',
+	'boat',
+	'bottle',
+	'bus',
+	'car',
+	'cat',
+	'chair',
+	'cow',
+	'dog',
+	'horse',
+	'motorbike',
+	'person',
+	'sheep',
+	'sofa',
+	'diningtable',
+	'pottedplant',
+	'train',
+	'tvmonitor',
+]
 
 def heatmap(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="", **kwargs):
 	fig = plt.figure()
@@ -25,4 +50,47 @@ def heatmap(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="", **k
 	plt.tick_params(which="minor", bottom=False, left=False)
 	return fig
 
-log = 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--runid', type=int, help='which run to use')
+parser.add_argument('--log', default=0, type=int, help='log to tensorboard? 0/1')
+parser.add_argument('--plotly', default=1, type=int, help='use plotly? 0/1')
+
+args, _ = parser.parse_known_args()
+
+logdir = f"run_{args.runid}"
+
+if args.log:
+	writer = SummaryWriter('fsimages/'+logdir)
+
+conf = torch.load('fsimages/'+logdir+'/conf.pt', map_location='cpu')
+conf_approx = torch.load('fsimages/'+logdir+'/conf_approx.pt', map_location='cpu')[1:, 1:]
+val_conf = torch.load('fsimages/'+logdir+'/val_conf.pt', map_location='cpu')
+val_conf_approx = torch.load('fsimages/'+logdir+'/val_conf_approx.pt', map_location='cpu')[1:, 1:]
+
+newconf = torch.zeros_like(conf)
+newconf[6:21, 6:21] = conf[1:16,1:16]
+newconf[0, 6:] = conf[0, 1:16]
+newconf[6:,0] = conf[1:16, 0]
+newconf[0,0] = conf[0,0]
+
+conf = torch.nn.functional.normalize(newconf, dim=1)
+val_conf = torch.nn.functional.normalize(val_conf, dim=1)
+
+pascal_classes = ['background'] + pascal_classes
+
+if args.plotly:
+	# px.imshow(conf, x=pascal_classes, y=pascal_classes).show()
+	px.imshow(conf, x=pascal_classes, y=pascal_classes, text_auto=True).show()
+	px.imshow(val_conf, x=pascal_classes, y=pascal_classes, text_auto=True).show()
+else:
+	heatmap(conf, pascal_classes, pascal_classes)
+	plt.show()
+	heatmap(conf, pascal_classes, pascal_classes, cmap='plasma')
+	plt.show()
+	heatmap(conf, pascal_classes, pascal_classes, cmap='PuRd')
+	plt.show()
+	heatmap(conf, pascal_classes, pascal_classes, cmap='YlGnBu')
+	plt.show()
+	heatmap(conf, pascal_classes, pascal_classes, cmap='cool')
+	plt.show()
